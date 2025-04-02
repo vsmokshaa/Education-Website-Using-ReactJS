e7e76927d2415a9b82dc3b45199be35e67f0296c
Mokshaa <vsmokshaa@gmail.com>
2025-04-02 22:57:04 +0530

    wordsalter

--- src/components/home/HAbout.jsx
+++ src/components/home/HAbout.jsx
@@ -9,7 +9,7 @@ const HAbout = () => {
     <>
       <section className='homeAbout'>
         <div className='container'>
-          <Heading subtitle='our courses' title='explore our popular online courses' />
+          <Heading subtitle='We Provide' title='explore our popular online tutors' />
 
           <div className='coursesCard'>
             {/* copy code form  coursesCard */}
--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
               </button>
               <button>
-                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+                VIEW PROFILES <i className='fa fa-long-arrow-alt-right'></i>
               </button>
             </div>
           </div>
